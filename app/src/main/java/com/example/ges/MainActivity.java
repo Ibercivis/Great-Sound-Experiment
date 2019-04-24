cf44dfa05ec89770b9434b74a76e6025e4eb3eed
jorge <jbarba@bifi.es>
2019-04-24 13:42:48 +0200

    v2

--- app/src/main/java/com/example/ges/MainActivity.java
+++ app/src/main/java/com/example/ges/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.ges;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         char hashashd;
         char prueba2;
+    }
+
+    public void aRegistro(View view) {
+        Intent intent = new Intent (this, Registro.class);
+        startActivityForResult(intent, 0);
+
+    }
+
+    public void aLogin(View view) {
+        Intent intent = new Intent (this, Login.class);
+        startActivityForResult(intent, 0);
+
     }
 }
